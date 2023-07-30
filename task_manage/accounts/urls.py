55ff210b16b3d7a72d33f52f192b9151a083dc54
bon-com <iwatamasateru1111@gmail.com>
2023-07-30 22:05:59 +0900

    ログイン、ログアウトまわり修正

--- task_manage/accounts/urls.py
+++ task_manage/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from . import views
-from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib import auth
 
 urlpatterns = [
-    path("", include("django.contrib.auth.urls")),
+    # path("", include("django.contrib.auth.urls")),
+    path("login/", auth.views.LoginView.as_view(), name="login"),
     path("signup/", views.SignupView.as_view(), name="signup"),
-    # path("logout/", LogoutView.as_view(), name="logout"),
+    path("logout/", auth.views.LogoutView.as_view(), name="logout"),
 ]
