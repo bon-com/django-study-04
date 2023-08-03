cf999514d7b899f3aaf7b4e96ed18767a69f5267
bon-com <iwatamasateru1111@gmail.com>
2023-08-03 21:17:50 +0900

    エラーメッセージなど整理

--- task_manage/todoapps/views.py
+++ task_manage/todoapps/views.py
@@ -1,8 +1,7 @@
 from django.urls import reverse_lazy
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Todo, TodoCategory
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView, UpdateView
+from . import models
+from django.views.generic.edit import CreateView, DeleteView
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -14,7 +13,7 @@ class ListTaskView(LoginRequiredMixin, ListView):
     # テンプレート名称
     template_name = "todo/top.html"
     # モデル名称
-    model = Todo
+    model = models.Todo
     # コンテキスト変数名
     context_object_name = "tasks"
     # ページネーション（1ページあたりの項目数）
@@ -22,7 +21,7 @@ class ListTaskView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         """一覧取得メソッドのオーバーライド"""
-        return Todo.objects.filter(status=0, user=self.request.user).order_by(
+        return models.Todo.objects.filter(status=0, user=self.request.user).order_by(
             "due_date"
         )
 
@@ -33,7 +32,7 @@ class DetailTaskView(DetailView):
     # テンプレート名称
     template_name = "todo/detail.html"
     # モデル名称
-    model = Todo
+    model = models.Todo
     # コンテキスト変数名
     context_object_name = "todo"
 
@@ -41,7 +40,7 @@ class DetailTaskView(DetailView):
 class TodoCreateView(CreateView):
     """タスク新規登録画面"""
 
-    model = Todo
+    model = models.Todo
     template_name = "todo/create.html"
     fields = ["task", "memo", "status", "due_date", "category"]
 
@@ -52,9 +51,9 @@ class TodoCreateView(CreateView):
     def get_success_url(self):
         return reverse_lazy("top")
 
-    def get_context_data(self):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context["categories"] = TodoCategory.objects.all()
+        context["categories"] = models.TodoCategory.objects.all()
         return context
 
 
@@ -62,29 +61,29 @@ class DeleteTaskView(DeleteView):
     """タスクの削除"""
 
     template_name = "todo/todo_confirm_delete.html"
-    model = Todo
+    model = models.Todo
     success_url = reverse_lazy("top")
 
 
 class TodoEditView(UpdateView):
     """タスクの編集"""
 
-    model = Todo
+    model = models.Todo
     fields = ["task", "memo", "due_date", "category", "status"]
     template_name = "todo/edit.html"
 
-    def get_context_data(self):
+    def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context["categories"] = TodoCategory.objects.all()
+        context["categories"] = models.TodoCategory.objects.all()
         return context
 
     def get_success_url(self):
-        return reverse_lazy("detail", context={"pk": self.object.pk})
+        return reverse_lazy("detail", kwargs={"pk": self.object.pk})
 
 
 def todo_complete(request, tid):
     """ステータス完了"""
-    todo = get_object_or_404(Todo, pk=tid)
+    todo = get_object_or_404(models.Todo, pk=tid)
     todo.status = 1
     todo.save()
     return HttpResponseRedirect(reverse_lazy("top"))
